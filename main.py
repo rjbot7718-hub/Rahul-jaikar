ba5912435bd71393479c5fe5b393826404fad9f8
rjbot7718-hub <rjbot7718@gmail.com>
2025-10-31 11:23:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -537,7 +537,7 @@ async def main_bot_logic() -> None:
             # --- FIX 2 (AttributeError) ---
             # filters.Video.ALL -> filters.VIDEO
             # filters.Document.ALL -> filters.DOCUMENT
-            GENERATE_POST: [MessageHandler(filters.VIDEO | filters.DOCUMENT, receive_file_and_ask_more_quality)],
+            GENERATE_POST: [MessageHandler(filters.VIDEO | filters.Document, receive_file_and_ask_more_quality)],
             CHECK_ANOTHER_EP: [CallbackQueryHandler(check_another_ep_handler, pattern="^(add_next_ep|add_new_season|generate_season_post|cancel_conv)$")]
         },
         fallbacks=[
